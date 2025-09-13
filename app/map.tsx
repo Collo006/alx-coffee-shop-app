85a6d6558f6d9b7f458becf201e1e471665d9965
Collins Kipruto <collinskipruto036@gmail.com>
2025-09-13 22:44:39 +0300

    delivery map done

--- app/map.tsx
+++ app/map.tsx
@@ -1,4 +1,5 @@
 import { Sora_400Regular, Sora_600SemiBold, Sora_700Bold, useFonts } from "@expo-google-fonts/sora";
+import { Ionicons } from "@expo/vector-icons";
 import axios from "axios";
 import { encode } from "base64-arraybuffer";
 import React, { useEffect, useState } from "react";
@@ -72,16 +73,41 @@ setMapUrl(base64Image);
                       {mapUrl && (
         <Image
           source={{ uri: mapUrl }}
-          style={{ width: 520, height: 440, borderRadius: 10 }}
+          style={{ width: 520, height: 570, borderRadius: 10 }}
         />
       )}
       <View className=" self-center mt-1">
         <View className="text-red-600 flex justify-center">
             <Text style={{fontFamily:"Sora_600SemiBold",fontSize:16}}>10 Minutes Left</Text>
-            <Text style={{fontFamily:"Sora_400Regular", fontSize:14}}>Delivery to JL.Kipyego</Text>
+            <Text style={{fontFamily:"Sora_400Regular", fontSize:14}} className="-ml-4 " >Delivery to JL.Kipyego</Text>
+        </View>
+      </View>
+
+              {/** Bars */}
+        <View className=" mt-4 h-7 w-96 self-center flex flex-row gap-5">
+          <View className="border border-[#36C07E] rounded-md mt-2 h-1 w-20 bg-[#36C07E]"></View>
+          <View className="border border-[#36C07E] rounded-md mt-2 h-1 w-20 bg-[#36C07E]"></View>
+          <View className="border border-[#36C07E] rounded-md mt-2 h-1 w-20 bg-[#36C07E]"></View>
+          <View className="border border-[#000] rounded-md mt-2 h-1 w-20"></View>
+        </View>
+        {/** delivery message */}
+        <View className="mt-5 pl-14 pr-2 flex flex-row border border-[#A2A2A2] rounded-xl gap-3 self-center h-20 w-96 ">
+          <View className="mt-3 border border-[#A2A2A2] rounded-xl h-12 w-12 -ml-12 "><Ionicons name="bicycle" size={35} color="#C67C4E"/></View>
+          <View className="mt-2">
+            <Text style={{fontFamily:"Sora_600SemiBold",fontSize:16}}>Delivered your Order</Text>
+            <Text style={{fontFamily:"Sora_400Regular",fontSize:14,color:"#A2A2A2"}}>We will deliver your goods to you in{"\n"} the shortest time possible</Text>
+          </View>
         </View>
 
-      </View>
+        <View className="mt-4 ml-16 pl-16 pt-2 flex-row justify-between gap-5 2] rounded-xl h-20 w-96">
+          <View className="border border-[#A2A2A2] rounded-lg h-14 w-14 -ml-5"><Ionicons name="person" size={40} color="#A2A2A2"/></View>
+          <View className=" pl-10 ">
+            <View><Text style={{fontFamily:"Sora_600SemiBold",fontSize:16}}>Brooklyn Simmons</Text></View>
+            <View><Text  style={{fontFamily:"Sora_400Regular",fontSize:12,color:"#A2A2A2"}}>Personal Courier</Text></View>
+          </View>
+          <View className="border border-[#A2A2A2] rounded-lg h-14 w-14 ml-12 "><Ionicons name="call" size={40} color="#A2A2A2"/></View>
+        </View>
+
 
             </SafeAreaView>
         </SafeAreaProvider>
